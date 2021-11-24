e4e2247d7cc8d2076c84120801a36f762d75a728
Anmol Chauhan <anmolchauhan977@gmail.com>
2021-11-24 20:05:28 +0530

    max30100 code

--- esp32code/esp32code.ino
+++ esp32code/esp32code.ino
@@ -1,8 +1,10 @@
 #pragma once
-#include<WiFi.h>
-#include<iostream>
-#include"values.h"
+#include <WiFi.h>
+#include <iostream>
+#include "values.h"
 #include <PubSubClient.h>
+#include <Wire.h>
+#include "MAX30100_PulseOximeter.h"
 
 const char *mqtt_broker = "192.168.0.104";
 const char *topic = "esp32/test";
@@ -10,7 +12,11 @@ const char *mqtt_username = "Chauhan";
 const char *mqtt_password = "public";
 const int mqtt_port = 1883;
 
-IPAddress server(192, 168, 1, 200);
+#define REPORTING_PERIOD_MS 1000
+float BPM, SpO2;
+
+PulseOximeter pox;
+uint32_t tsLastReport = 0;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -21,8 +27,8 @@ char *PASS = WIFIPASS;
 void ConnectToWiFi()
 {
   WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID,PASS);
-  Serial.print("Connecting to Wifi \n"); 
+  WiFi.begin(SSID, PASS);
+  Serial.print("Connecting to Wifi \n");
   while (WiFi.status() != WL_CONNECTED)
   {
     Serial.print("connecting... \n");
@@ -32,40 +38,76 @@ void ConnectToWiFi()
   Serial.println(WiFi.localIP());
 }
 
-void callback(char *topic, byte *payload, unsigned int length) {
- Serial.print("Message arrived in topic: ");
- Serial.println(topic);
- Serial.print("Message:");
- for (int i = 0; i < length; i++) {
-     Serial.print((char) payload[i]);
- }
- Serial.println();
- Serial.println("-----------------------");
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  Serial.println("-----------------------");
 }
 
-void connecttoMQTT(){
-   client.setServer(mqtt_broker,mqtt_port);
+void connecttoMQTT()
+{
+  client.setServer(mqtt_broker, mqtt_port);
   client.setCallback(callback);
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     String client_id = "esp32-client-";
     client_id += String(WiFi.macAddress());
     Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
-    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
-        Serial.println("Public mosquitto mqtt broker connected");
-    } else {
-        Serial.print("failed with state ");
-        Serial.print(client.state());
-        delay(2000);
+    if (client.connect(client_id.c_str(), mqtt_username, mqtt_password))
+    {
+      Serial.println("Public mosquitto mqtt broker connected \n");
     }
-}
+    else
+    {
+      Serial.print("failed with state ");
+      Serial.println(client.state());
+      delay(2000);
+    }
+  }
 }
 
-void setup() {
+void onBeatDetected()
+{
+    Serial.println("Beat Detected!");
+}
+
+void setup()
+{
+  Wire.begin();
   Serial.begin(512000);
+  while(!Serial);
   ConnectToWiFi();
   connecttoMQTT();
+  if (!pox.begin())
+    {
+         Serial.println("FAILED");
+         for(;;);
+    }
+    else
+    {
+         Serial.println("SUCCESS");
+         pox.setOnBeatDetectedCallback(onBeatDetected);
+    }
 }
 
-void loop() {
-
+void loop()
+{
+   pox.update();
+   BPM = pox.getHeartRate();
+    if (millis() - tsLastReport > REPORTING_PERIOD_MS)
+    {
+        Serial.print("Heart rate:");
+        Serial.print(BPM);
+        Serial.print(" bpm / SpO2:");
+        Serial.print(SpO2);
+        Serial.println(" %");
+        tsLastReport = millis();
+    }
 }

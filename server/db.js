9280860522078cbd93cda630c437095770aeb892
Shivam Dureja <shivamdureja13@gmail.com>
2021-12-21 20:02:12 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,14 +1,12 @@
-const {Pool,Client} = require('pg');
+const { Pool } = require("pg");
 const cred = {
-    user:"postgres",
-    password:"shivam",
-    host:"localhost",
-    port:5432,
-    database:"covmon"
-}
+  user: "postgres",
+  password: "shivam",
+  host: "localhost",
+  port: 5432,
+  database: "covmon",
+};
+
 const pool = new Pool(cred);
-
-
-
 
 module.exports = pool;

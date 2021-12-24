48e608b698d1c645cad46998611e374ab996696b
Shivam Dureja <shivamdureja13@gmail.com>
2021-12-24 15:02:42 +0530

    ph number validations added

--- server/routes/patientValidations.js
+++ server/routes/patientValidations.js
@@ -4,7 +4,7 @@ const patientSignupValidation = data => {
      const schema = joi.object({
           fullName: joi.string().required(),
           email: joi.string().email().required(),
-          phoneNumber: joi.string().required().max(10),
+          phoneNumber: joi.string().required().max(10).min(10),
           password: joi.string().required(),
           dob: joi.string().required(),
           roomNo: joi.string()
@@ -16,7 +16,7 @@ const patientLogInValidation = data => {
      const schema = joi.object({
           fullName: joi.string(),
           email: joi.string().email().required(),
-          phoneNumber: joi.string(),
+          phoneNumber: joi.string().max(10).min(10),
           password: joi.string().required(),
      })
      return schema.validate(data);

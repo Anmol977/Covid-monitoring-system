3097538b001b6ffcd4c814c66e1567a764ccc274
Anmol Chauhan <anmolchauhan977@gmail.com>
2021-12-24 21:53:27 +0530

    body parser

--- server/index.js
+++ server/index.js
@@ -4,20 +4,23 @@ const db = require('./db');
 const mqtt = require('./mqtt/connectMqtt');
 const logger = require('./logger');
 const userRoutes = require('./routes/patientRoutes');
-const doctorRoutes = require('./routes/doctorRoutes')
+const doctorRoutes = require('./routes/doctorRoutes');
+var bodyParser = require('body-parser');
 
 const app = express();
-app.use(express.json());
 
-(async () => {
-  try {
-    await db.migrate.latest().then(() => {
-      logger.info('database migrations complete...');
-    });
-  } catch (e) {
-    console.log(e);
-  }
-})();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+  (async () => {
+    try {
+      await db.migrate.latest().then(() => {
+        logger.info('database migrations complete...');
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  })();
 
 app.listen(5000, (req, res) => {
   logger.info("server running on port : 5000")
